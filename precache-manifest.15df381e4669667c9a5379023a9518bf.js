63a6484d92b0e7392fab42432011fab66732cf13
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2020-07-31 15:16:26 -0400

    Updates

---
+++ precache-manifest.15df381e4669667c9a5379023a9518bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f82ab661725cd834d36659d64a0503ce",
+    "url": "/employee_directory/index.html"
+  },
+  {
+    "revision": "e1dab51416b09ab22cb6",
+    "url": "/employee_directory/static/js/2.12ab66de.chunk.js"
+  },
+  {
+    "revision": "0a572ad323474bafc6a871b6336141bd",
+    "url": "/employee_directory/static/js/2.12ab66de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "deb2c3ec3adf8c3f022e",
+    "url": "/employee_directory/static/js/main.df14b84e.chunk.js"
+  },
+  {
+    "revision": "1b93aa557ffa08feb7ac",
+    "url": "/employee_directory/static/js/runtime-main.84683fd0.js"
+  }
+]);
--- src/utils/API.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-const URL = "https://randomuser.me/api/?results=100&inc=picture,name,phone,email,dob";
-
-export default {
-  getUsers: () => {
-    return axios.get(URL);
-  },
-};
