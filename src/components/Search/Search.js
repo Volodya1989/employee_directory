45140d087f4cc3a9a66c7707e36b4af822500b58
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2020-08-02 06:58:50 -0400

    final comments added

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,15 +1,17 @@
 import React from "react";
 import "./search.styles.css";
 
-export default function Search({ onSearch }) {
+const Search = ({ onSearch }) => {
   return (
-      <div className="searchField raw form-inline justify-content-center">
-        <input
-          type="text"
-          className="search  "
-          placeholder="search by name"
-          onChange={onSearch}
-        />
-      </div>
+    <div className="searchField raw form-inline justify-content-center">
+      <input
+        type="text"
+        className="search  "
+        placeholder="search by name"
+        //here we are passing method that is responsible for narrowing list of employees according to user's input in search field
+        onChange={onSearch}
+      />
+    </div>
   );
-}
+};
+export default Search;
